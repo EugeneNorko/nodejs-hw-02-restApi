cd32083aa610784b8d053cae3edcf9bcb035adbd
EugeneNorko <zhenia020221@gmail.com>
2022-10-24 12:35:26 +0300

    addAuthUser

---
+++ models/user.js
@@ -0,0 +1,54 @@
+const { Schema, model } = require("mongoose");
+const Joi = require("joi");
+const { handleSaveErrors } = require("../helpers");
+
+const emailRegexp = /^[a-z0-9]+@[a-z]+\.[a-z]{2,3}$/;
+const userSchema = new Schema(
+    {
+        password: {
+            type: String,
+            required: [true, "Set password for user"],
+        },
+        email: {
+            type: String,
+            required: [true, "Email is required"],
+            unique: true,
+            match: emailRegexp,
+        },
+        subscription: {
+            type: String,
+            enum: ["starter", "pro", "business"],
+            default: "starter",
+        },
+        token: {
+            type: String,
+            default: "",
+        },
+    },
+    { versionKey: false, timestamps: true }
+);
+
+userSchema.post("save", handleSaveErrors);
+
+const registerSchema = Joi.object({
+    password: Joi.string().required(),
+    email: Joi.string().pattern(emailRegexp).required(),
+    subscription: Joi.string(),
+});
+
+const loginSchema = Joi.object({
+    password: Joi.string().required(),
+    email: Joi.string().pattern(emailRegexp).required(),
+});
+
+const schemas = {
+    registerSchema,
+    loginSchema,
+};
+
+const User = model("user", userSchema);
+
+module.exports = {
+    User,
+    schemas,
+};
